32a5e407b8d027e7d136af268b9a15d8ed6f1ce2
Celina Jackson <celinanjackson@gmail.com>
2020-09-09 20:28:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,5 +19,11 @@ class App < Sinatra::Base
   end.first
   erb :'/medicines/show.html'
   end
+  
+  all_the_medicines = [
+  <Medicine:0x007fb739b1af88 @id=1, @name="penicillin" @group="antibiotic">,
+  <Medicine:0x007fb739b1af88 @id=2, @name="advil" @group="anti-inflammatory">,
+  <Medicine:0x007fb739b1af88 @id=3, @name="benadryl" @group="anti-histamine">
+]
 
 end
